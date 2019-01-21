f04378e090cf2ea48994c400e82a407e9d8dfed3
jonathan08 <jonathan@cosmunsoftwares.com.br>
2019-01-21 11:54:27 -0200

    developing firebase

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,33 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  $scope.user = null;
+  $scope.user_id = 't9l18UKPut3npvMzNNjC';
+  $scope.hasMessage = false;
 
-  // Form data for the login modal
-  $scope.loginData = {};
+  $scope.setAvatar = function() {
+    console.log('alskdjaslkdjlas')
+  };
+})
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
+.controller('MessageCtrl', function($scope, $ionicModal) {
+
+  firebase.database().ref('messages/').then(function(data){
+    console.log(data)
+  })
+
+  $ionicModal.fromTemplateUrl('templates/message/detail.html', {
     scope: $scope
   }).then(function(modal) {
     $scope.modal = modal;
   });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
+  $scope.close = function() {
     $scope.modal.hide();
   };
 
-  // Open the login modal
-  $scope.login = function() {
+  $scope.goToDetail = function() {
     $scope.modal.show();
   };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
 })
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('MessageCtrl', function($scope, $stateParams) {
-});
---
+++ www/js/env.js
@@ -0,0 +1,12 @@
+(function (window) {
+  window.__env = window.__env || {};
+
+  window.__env.apiKey = "AIzaSyDcFg_Qg8ardx7PcLzC9GAzayLxXLs5UQg";
+  window.__env.authDomain = "youper-8346b.firebaseapp.com";
+  window.__env.databaseURL = "https://youper-8346b.firebaseio.com";
+  window.__env.projectId = "youper-8346b";
+  window.__env.storageBucket = "youper-8346b.appspot.com";
+  window.__env.messagingSenderId = "1009809031147";
+
+  window.__env.enableDebug = true;
+}(this));
